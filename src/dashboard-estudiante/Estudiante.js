1bb632fa228a21d541cdda9464e6104062020b0b
CristobalMS2017 <45274450+CristobalMS2017@users.noreply.github.com>
2021-04-15 20:28:07 -0400

    Actualizado con grommet

--- src/dashboard-estudiante/Estudiante.js
+++ src/dashboard-estudiante/Estudiante.js
@@ -1,10 +1,17 @@
-import { Accordion, Card, Col, Container, Row } from 'react-bootstrap'
-import { BsFillCaretDownFill } from "react-icons/bs";
+
+
 import Documento from './extras/Documento'
 import Practica from './extras/Practica'
 import React from 'react'
 import axios from 'axios'
+import {
+  Accordion,
+  AccordionPanel,
+  Text,
+  Box,
+  Heading
 
+} from 'grommet';
 class Estudiante extends React.Component {
   constructor(props) {
     super(props)
@@ -44,46 +51,24 @@ class Estudiante extends React.Component {
 
   render(){
     return (
-      <div className="m-2">
-        <h3>Hola! {this.state.user.name}</h3>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipiscing elit dui, fringilla facilisis at montes suscipit rhoncus ultrices, cursus augue primis auctor cum tortor litora. Ligula scelerisque orci himenaeos blandit sagittis curabitur quam, mauris primis phasellus natoque dapibus tempus, ac potenti augue egestas torquent laoreet.
-        </p>
         <Accordion>
-          <React.StrictMode>
-            <Card>
-                <Accordion.Toggle as={Card.Header} eventKey="0">
-                  <Container>
-                    <Row>
-                      <Col className="pull-xs-right" xs={10}>Documentos</Col>
-                      <Col className="pull-xs-left" xs={2}><BsFillCaretDownFill/></Col>
-                    </Row>
-                  </Container>
-                </Accordion.Toggle>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body>
-                    <Documento docs={this.state.documentos}/>
-                  </Card.Body>
-                </Accordion.Collapse>
-            </Card>
-            <Card>
-                <Accordion.Toggle as={Card.Header} eventKey="1">
-                  <Container>
-                    <Row>
-                      <Col className="pull-xs-right" xs={10}>Prácticas</Col>
-                      <Col className="pull-xs-left" xs={2}><BsFillCaretDownFill/></Col>
-                    </Row>
-                  </Container>
-                </Accordion.Toggle>
-                <Accordion.Collapse eventKey="1">
-                  <Card.Body>
-                    <Practica practicas={this.state.practicas}/>
-                  </Card.Body>
-                </Accordion.Collapse>
-            </Card>
-            </React.StrictMode>
-        </Accordion>
-      </div>
+        <Heading margin="none" >Hola! {this.state.user.name}</Heading>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipiscing elit dui, fringilla facilisis at montes suscipit rhoncus ultrices, cursus augue primis auctor cum tortor litora. Ligula scelerisque orci himenaeos blandit sagittis curabitur quam, mauris primis phasellus natoque dapibus tempus, ac potenti augue egestas torquent laoreet.
+        </Text>
+        <br/>
+      <AccordionPanel label="Documentos">
+          <Box pad="medium" background="light-2">
+
+          <Documento docs={this.state.documentos}/>
+          </Box>
+      </AccordionPanel>
+      <AccordionPanel label="Prácticas">
+          <Box pad="medium" background="light-2">
+          <Practica practicas={this.state.practicas}/>
+          </Box>
+      </AccordionPanel>
+      </Accordion>
     );
   }
 }

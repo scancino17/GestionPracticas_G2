d52c7fd5e0c89cd0e40905bcf3fa0a3fe1602448
Carlos Daniel Ríos Moya <cd.riosmoya@outlook.com>
2021-04-16 16:11:10 -0400

    Integración Firebase y Firebase Auth

---
+++ functions/index.js
@@ -0,0 +1,26 @@
+const functions = require("firebase-functions");
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+exports.processSignUp = functions.auth.user().onCreate((user) => {
+    if (user.email && user.email.endsWith('@admintest.com')) {
+        const customClaims = {
+            admin: true
+        };
+        return admin.auth().setCustomUserClaims(user.uid, customClaims).then(() => {
+            const metadataRef = admin.database().ref("metadata/" + user.uid);
+            return metadataRef.set({refreshTime: new Date().getTime()});
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+});
--- src/App.js
+++ src/App.js
@@ -1,5 +1,69 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  const signInWithEmailAndPasswordHandler = (event, email, password) => {
+    event.preventDefault();
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(error => {
+      setError("Error signing in with password and email!");
+      console.error("Error signing in with password and email", error)
+    }).then(() => {
+      const token = firebase.auth().currentUser.getIdTokenResult();
+      console.log("Successfully logged user!")
+      console.log(token);
+    });
+  };
+
+  const onChangeHandler = (event) => {
+    const { name, value } = event.currentTarget;
+    if (name === 'userEmail') {
+        setEmail(value);
+    } else if (name === 'userPassword') {
+        setPassword(value);
+    }
+  };
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="userEmail">
+          Email:
+        </label>
+        <input 
+          type="email"
+          name="userEmail"
+          value={email}
+          placeholder="E.g: jose123@gmail.com"
+          id="userEmail"
+          onChange={(event) => onChangeHandler(event)}
+        />
+        <label htmlFor="userPassword" className="block">
+          Password:
+        </label>
+        <input
+          type="password"
+          name="userPassword"
+          value={password}
+          placeholder="Your password"
+          id="userPassword"
+          onChange={(event) => onChangeHandler(event)}
+        />
+        <button
+          onClick={(event) => signInWithEmailAndPasswordHandler(event, email, password)}
+        >
+        Sign in
+        </button>
+      </form>
+    </div>
+  );
+}
 
 function App() {
   return (
@@ -18,6 +82,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <SignIn></SignIn>
     </div>
   );
 }

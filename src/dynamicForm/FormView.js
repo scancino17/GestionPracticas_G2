9cd6d8a3814d32acebcc3a59a785f6a530781b97
ThePina <43052176+ThePina@users.noreply.github.com>
2021-05-29 21:20:49 -0400

    removed card

--- src/dynamicForm/FormView.js
+++ src/dynamicForm/FormView.js
@@ -32,111 +32,105 @@ function FormView(props) {
   };
 
   return (
-    <Card>
-      <CardContent>
-        <Grid container direction='column' spacing={5}>
-          {props.form
-            ? props.form.map((element, index) =>
-                element.type === 'Select' ? (
-                  <Grid item>
-                    <FormControl fullWidth>
-                      <InputLabel>{element.name}</InputLabel>
-                      <Select
-                        fullWidth
-                        value={element.value}
-                        open={element.open}
-                        onClose={() => updateItem(index, 'open', false)}
-                        onOpen={() => updateItem(index, 'open', true)}
-                        onChange={(e) =>
-                          updateItem(index, 'value', e.target.value)
-                        }>
-                        <MenuItem value={''}>None</MenuItem>
-                        {element.options.map((option) => (
-                          <MenuItem value={option}>{option}</MenuItem>
-                        ))}
-                      </Select>
-                    </FormControl>
-                  </Grid>
-                ) : element.type === 'Input' ? (
-                  <Grid item xs>
-                    <TextField
-                      fullWidth
-                      variant='outlined'
-                      xs={12}
-                      required
-                      id='standard-required'
-                      label={element.name}
-                      defaultValue={element.value}
-                      onChange={(e) =>
-                        updateItem(index, 'value', e.target.value)
-                      }
-                    />
-                  </Grid>
-                ) : element.type === 'File' ? (
-                  <Grid item xs>
-                    <div>
-                      <Typography variant='h6' gutterBottom>
-                        {element.name}
-                      </Typography>
-                      <input
-                        style={{ display: 'none' }}
-                        accept='image/*'
-                        id='contained-button-file'
-                        multiple
-                        type='file'
-                      />
-                      <label htmlFor='contained-button-file'>
-                        <Button
-                          variant='contained'
-                          color='primary'
-                          component='span'>
-                          Upload
-                        </Button>
-                      </label>
-                    </div>
-                  </Grid>
-                ) : element.type === 'Header' ? (
-                  <Grid item xs>
-                    <Typography variant='h3' gutterBottom>
-                      {element.name}
-                    </Typography>
-                  </Grid>
-                ) : element.type === 'Space' ? (
-                  <Grid item xs>
-                    <Typography variant='h2' gutterBottom>
-                      {' '}
-                    </Typography>
-                  </Grid>
-                ) : element.type === 'predefinido' ? (
-                  element.type2 === 'Ciudad' ? (
-                    <Grid item xs>
-                      <h1>predef</h1>
-                      <Selector
-                        valueinner={element.value}
-                        camp={element.type2}
-                        onParentChange={(newValue) => {
-                          updateItem(index, 'value', newValue.label);
-                        }}
-                      />
-                    </Grid>
-                  ) : element.type2 === 'Compania' ? (
-                    <Grid item xs>
-                      <h1>Compañia</h1>
-                      <Selector
-                        valueinner={element.value}
-                        camp={element.type2}
-                        onParentChange={(newValue) => {
-                          updateItem(index, 'value', newValue.label);
-                        }}
-                      />
-                    </Grid>
-                  ) : null
-                ) : null
-              )
-            : null}
-        </Grid>
-      </CardContent>
-    </Card>
+    <Grid container direction='column' spacing={5}>
+      {props.form
+        ? props.form.map((element, index) =>
+            element.type === 'Select' ? (
+              <Grid item>
+                <FormControl fullWidth>
+                  <InputLabel>{element.name}</InputLabel>
+                  <Select
+                    fullWidth
+                    value={element.value}
+                    open={element.open}
+                    onClose={() => updateItem(index, 'open', false)}
+                    onOpen={() => updateItem(index, 'open', true)}
+                    onChange={(e) =>
+                      updateItem(index, 'value', e.target.value)
+                    }>
+                    <MenuItem value={''}>None</MenuItem>
+                    {element.options.map((option) => (
+                      <MenuItem value={option}>{option}</MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Grid>
+            ) : element.type === 'Input' ? (
+              <Grid item xs>
+                <TextField
+                  fullWidth
+                  variant='outlined'
+                  xs={12}
+                  required
+                  id='standard-required'
+                  label={element.name}
+                  value={element.value}
+                  onChange={(e) => updateItem(index, 'value', e.target.value)}
+                />
+              </Grid>
+            ) : element.type === 'File' ? (
+              <Grid item xs>
+                <div>
+                  <Typography variant='h6' gutterBottom>
+                    {element.name}
+                  </Typography>
+                  <input
+                    style={{ display: 'none' }}
+                    accept='image/*'
+                    id='contained-button-file'
+                    multiple
+                    type='file'
+                  />
+                  <label htmlFor='contained-button-file'>
+                    <Button
+                      variant='contained'
+                      color='primary'
+                      component='span'>
+                      Upload
+                    </Button>
+                  </label>
+                </div>
+              </Grid>
+            ) : element.type === 'Header' ? (
+              <Grid item xs>
+                <Typography variant='h3' gutterBottom>
+                  {element.name}
+                </Typography>
+              </Grid>
+            ) : element.type === 'Space' ? (
+              <Grid item xs>
+                <Typography variant='h2' gutterBottom>
+                  {' '}
+                </Typography>
+              </Grid>
+            ) : element.type === 'predefinido' ? (
+              element.type2 === 'Ciudad' ? (
+                <Grid item xs>
+                  <h1>predef</h1>
+                  <Selector
+                    valueinner={element.value}
+                    camp={element.type2}
+                    onParentChange={(newValue) => {
+                      updateItem(index, 'value', newValue.label);
+                    }}
+                  />
+                </Grid>
+              ) : element.type2 === 'Compania' ? (
+                <Grid item xs>
+                  <h1>Compañia</h1>
+                  <Selector
+                    valueinner={element.value}
+                    camp={element.type2}
+                    onParentChange={(newValue) => {
+                      updateItem(index, 'value', newValue.label);
+                    }}
+                  />
+                </Grid>
+              ) : null
+            ) : null
+          )
+        : null}
+    </Grid>
   );
 }
 export default FormView;

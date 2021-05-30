ddcf799d878efdde17a1be5081f4f2099663b2f8
Carlos Daniel Ríos Moya <cd.riosmoya@outlook.com>
2021-05-30 02:07:02 -0400

    Modified things in FormCheck

--- src/dynamicForm/FormCheck.js
+++ src/dynamicForm/FormCheck.js
@@ -1,5 +1,5 @@
-import { Button, Grid, makeStyles, Typography } from '@material-ui/core';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, Grid, Typography } from '@material-ui/core';
 import { useParams } from 'react-router-dom';
 import { db } from '../firebase';
 
@@ -12,40 +12,27 @@ function FormCheck() {
       .get()
       .then((doc) => {
         const data = doc.data();
-        console.log(data);
-        if (data) {
-          setIntership(data.form);
-        }
+        if (data) setIntership(data.form);
       });
   }, []);
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex'
-    },
-    content: {
-      flexGrow: 1,
-      paddingTop: theme.spacing(14)
-    }
-  }));
-  const classes = useStyles();
+
   return (
-    <div className={classes.content}>
+    <>
       <Grid container direction='column' spacing={5}>
         {intership.map((camp) =>
-          camp.form.map(
-            (form) =>
-              form.type !== 'Space' && (
-                <Grid item>
-                  <Typography variant='h6'>
-                    {form.name}={form.value}
-                  </Typography>
-                </Grid>
-              )
-          )
+          camp.form.map((form) => {
+            form.type !== 'Space' && (
+              <Grid item>
+                <Typography variant='h6'>
+                  {form.name}={form.value}
+                </Typography>
+              </Grid>
+            );
+          })
         )}
       </Grid>
       <Button onClick={() => console.log(intership)}> intership</Button>
-    </div>
+    </>
   );
 }
 export default FormCheck;

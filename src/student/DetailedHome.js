ed3078c313aea8098ce6fdf8bd17f0e1dbf6593f
Sebastian Cancino <sebcan1998@gmail.com>
2021-06-30 03:06:28 -0400

    Implemented state-banner

--- src/student/DetailedHome.js
+++ src/student/DetailedHome.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ToDoList from './extras/ToDoList';
 import Stories from './extras/Stories';
+import StateBanner from './extras/StateBanner';
 import { Container, Grid } from '@material-ui/core';
 
 function DetailedHome({ done, reason }) {
   return (
     <Container style={{ padding: '2rem' }}>
       <Grid container direction='column' spacing={6}>
+        {/* Statebanner tiene un grid item adentro*/}
+        <StateBanner />
         <Grid item>
           <ToDoList done={done} reason={reason} />
         </Grid>
